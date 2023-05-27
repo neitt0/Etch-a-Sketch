f7b0163f87943ef047a55500e88763fc91493910
Thien Nguyen <neiht0805@gmail.com>
2023-05-27 19:55:04 -0400

    increase darkness per grid pixel

--- script.js
+++ script.js
@@ -9,36 +9,12 @@ createGrid(gridSize);
 // BUTTON FUNCTIONALITY
 button.addEventListener("click", () => {
   gridSize = prompt("Enter your desired grid size", 16);
-  
+
   if (gridSize > 100) {
     gridSize = 100;
-    alert("Your size has been capped to 100 due to performance issues")
+    alert("Your size has been capped to 100 due to performance issues");
   } else if (!gridSize) gridSize = 16;
   createGrid(gridSize);
-});
-
-// HOVER
-const hoverDivs = document.querySelectorAll(".hoverDiv");
-
-let colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
-let randomNum = 0;
-
-container.addEventListener("mouseenter", () => {
-  randomNum = getRandomNum(0, colors.length);
-})
-
-hoverDivs.forEach((div) => {
-  div.addEventListener("mouseenter", () => {
-    div.style.transition = "";
-    div.style.backgroundColor = colors[randomNum];
-  });
-});
-
-hoverDivs.forEach((div) => {
-  div.addEventListener("mouseleave", () => {
-    div.style.transition = "all 5s";
-    div.style.backgroundColor = "";
-  });
 });
 
 // RANDOM NUMBER GEN
@@ -54,10 +30,10 @@ function createGrid(size) {
   for (let i = 0; i < size; i++) {
     let columnDiv = document.createElement("div");
     columnDiv.style.cssText = `
-      display: flex;
-      flex-direction: column;
-      gap: 1px;
-      flex: 1;
+    display: flex;
+    flex-direction: column;
+    gap: 1px;
+    flex: 1;
     `;
 
     for (let j = 0; j < size; j++) {
@@ -69,4 +45,34 @@ function createGrid(size) {
 
     container.appendChild(columnDiv);
   }
+
+  // HOVER
+  
+  const hoverDivs = document.querySelectorAll(".hoverDiv");
+  
+  container.addEventListener("mouseenter", () => {
+    let colorValue = getRandomNum(0, 360);
+    let saturation = 100;
+    let lightness = 50;
+
+    hoverDivs.forEach((div) => {
+      
+      div.addEventListener("mouseenter", () => {
+        let color = `hsl(${colorValue}, ${saturation}%, ${lightness}%)`
+        
+        div.style.transition = "";
+        div.style.backgroundColor = color;
+
+        saturation -= 2;
+        lightness -= 1;
+      });
+    });
+
+    hoverDivs.forEach((div) => {
+      div.addEventListener("mouseleave", () => {
+        div.style.transition = "all 5s";
+        div.style.backgroundColor = "";
+      });
+    });
+  });
 }
