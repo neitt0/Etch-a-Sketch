fc1f99d96f696224b4c65d5e7701cdf701b092b3
Thien Nguyen <neiht0805@gmail.com>
2023-05-27 13:40:59 -0400

    added button to change size

--- script.js
+++ script.js
@@ -1,36 +1,58 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 16; i++) {
-  let columnDiv = document.createElement("div");
-  columnDiv.style.cssText = `
-    display: flex;
-    flex-direction: column;
-    gap: 1px; flex: 1;
+const button = document.querySelector("button");
+let gridSize = 16;
+
+// INITIALIZATION
+createGrid(gridSize);
+
+// BUTTON FUNCTIONALITY
+button.addEventListener("click", () => {
+  gridSize = prompt("Enter your desired grid size", 16);
+
+  if (gridSize > 100) {
+    gridSize = 100;
+    alert("Your size has been capped to 100 due to performance issues")
+  } else if (!gridSize) gridSize = 16;
+  createGrid(gridSize);
+});
+
+// CREATING GRID
+function createGrid(size) {
+  container.innerHTML = "";
+
+  for (let i = 0; i < size; i++) {
+    let columnDiv = document.createElement("div");
+    columnDiv.style.cssText = `
+      display: flex;
+      flex-direction: column;
+      gap: 1px; flex: 1;
     `;
-  // columnDiv.style.backgroundColor = "yellow";
-  
-  for (let j = 0; j < 16; j++) {
-    let rowDiv = document.createElement("div");
-    rowDiv.classList.add("hoverDiv")
 
-    columnDiv.appendChild(rowDiv)
+    for (let j = 0; j < size; j++) {
+      let rowDiv = document.createElement("div");
+      rowDiv.classList.add("hoverDiv");
+
+      columnDiv.appendChild(rowDiv);
+    }
+
+    container.appendChild(columnDiv);
   }
-
-  container.appendChild(columnDiv);
+  
+  const hoverDivs = document.querySelectorAll(".hoverDiv");
+  
+  // HOVER
+  hoverDivs.forEach((div) => {
+    div.addEventListener("mouseenter", () => {
+      div.style.transition = "";
+      div.style.backgroundColor = "black";
+    });
+  });
+  
+  hoverDivs.forEach((div) => {
+    div.addEventListener("mouseleave", () => {
+      div.style.transition = "all 1s";
+      div.style.backgroundColor = "";
+    });
+  });
 }
-
-const hoverDivs = document.querySelectorAll(".hoverDiv")
-
-hoverDivs.forEach(div => {
-  div.addEventListener("mouseenter", () => {
-    div.style.transition = ""
-    div.style.backgroundColor = "black"
-  })
-})
-
-hoverDivs.forEach(div => {
-  div.addEventListener("mouseleave", () => {
-    div.style.transition = "all 1s"
-    div.style.backgroundColor = ""
-  })
-})
