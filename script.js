4fccf58fee69479c76e4bd841d85c56c994bed97
Thien Nguyen <neiht0805@gmail.com>
2023-05-27 19:10:44 -0400

    added a color generator for every passing of the grid

--- script.js
+++ script.js
@@ -9,13 +9,43 @@ createGrid(gridSize);
 // BUTTON FUNCTIONALITY
 button.addEventListener("click", () => {
   gridSize = prompt("Enter your desired grid size", 16);
-
+  
   if (gridSize > 100) {
     gridSize = 100;
     alert("Your size has been capped to 100 due to performance issues")
   } else if (!gridSize) gridSize = 16;
   createGrid(gridSize);
 });
+
+// HOVER
+const hoverDivs = document.querySelectorAll(".hoverDiv");
+
+let colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+let randomNum = 0;
+
+container.addEventListener("mouseenter", () => {
+  randomNum = getRandomNum(0, colors.length);
+})
+
+hoverDivs.forEach((div) => {
+  div.addEventListener("mouseenter", () => {
+    div.style.transition = "";
+    div.style.backgroundColor = colors[randomNum];
+  });
+});
+
+hoverDivs.forEach((div) => {
+  div.addEventListener("mouseleave", () => {
+    div.style.transition = "all 5s";
+    div.style.backgroundColor = "";
+  });
+});
+
+// RANDOM NUMBER GEN
+// "small" and "large" args for the purpose of learning
+function getRandomNum(smallN, largeN) {
+  return Math.floor(Math.random() * (largeN - smallN)) + smallN;
+}
 
 // CREATING GRID
 function createGrid(size) {
@@ -26,7 +56,8 @@ function createGrid(size) {
     columnDiv.style.cssText = `
       display: flex;
       flex-direction: column;
-      gap: 1px; flex: 1;
+      gap: 1px;
+      flex: 1;
     `;
 
     for (let j = 0; j < size; j++) {
@@ -38,21 +69,4 @@ function createGrid(size) {
 
     container.appendChild(columnDiv);
   }
-  
-  const hoverDivs = document.querySelectorAll(".hoverDiv");
-  
-  // HOVER
-  hoverDivs.forEach((div) => {
-    div.addEventListener("mouseenter", () => {
-      div.style.transition = "";
-      div.style.backgroundColor = "black";
-    });
-  });
-  
-  hoverDivs.forEach((div) => {
-    div.addEventListener("mouseleave", () => {
-      div.style.transition = "all 1s";
-      div.style.backgroundColor = "";
-    });
-  });
 }
